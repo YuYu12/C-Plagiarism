4c8d48d06d89b660aefc27ede864e0437d9aaadb
YuYu12 <yyu17@stevens.edu>
2016-11-10 17:01:25 -0500

    Add files via upload

--- Plagiarism.java
+++ Plagiarism.java
@@ -1,42 +1,77 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 /*
- * work with download group
+ * author: Yu Yu 
+ * modified： 
  */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Plagiarism{
-	private String tokens;
-	private BigInteger intSimHash;
-	private String strSimHash;
-	private int hashbits = 64;
-	public Plagiarism(String tokens) throws IOException{
-		this.tokens = tokens;
+	public ArrayList<String> List = new ArrayList<String>();
+	public ArrayList<String> List2 = new ArrayList<String>();
+	//read file by lines
+	public String readFile(String fileName){
+		String res = "";
+		File file = new File(fileName);
+		BufferedReader reader = null;
+		try{
+			reader = new BufferedReader(new FileReader(file));
+		String tempString = null;
+		int line = 1;
+		while ((tempString = reader.readLine())!=null){
+			//System.out.println(line+":"+tempString);
+			res += tempString.trim();
+			line++;
+		}
 		
-		
+		reader.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			if (reader !=null){
+				try{
+					reader.close();
+				}catch(IOException e1){
+					
+				}
+			}
+		}
+		return res;
 		
 	}
-	public Plagiarism(String tokens, int hashbits) throws IOException{
-		this.tokens = tokens;
-		this.hashbits = hashbits;
-		
-		
-		
-		
-	}		
-	HashMap<String, Integer> wordMap = new HashMap<String,Integer>();
+
 	
-	public BigInteger plagiarism() throws IOException{
-		int[] v= new int[this.hashbits];
+	public static void main(String args[]){
+		Ss s = new Ss();
+		int size = 0;
 		
+		String path1 = "/Users/Yuyu/Downloads/PlagarismDetector.java";
+		String path2 = "/Users/Yuyu/Downloads/Complex-1.java";
+		Plagiarism p = new Plagiarism();
+		String s1 = p.readFile(path1);
+		String s2 = p.readFile(path2);
+		List<String> l = s.getSameStr(s1, s2);
+		for(String str : l){
+			System.out.println(str.length());
+			System.out.println("plagiarism ratio"+(double)str.length()*100/s1.length()+"%");
+		}
 		
-		
-	
-		
-	}
-	public static void main(String[] args) throws IOException{
-		    
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
